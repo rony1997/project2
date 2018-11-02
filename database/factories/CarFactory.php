2cec19c3fa59cadf336c03b0e6ae52cce71c11cd
rony1997 <rad59@njit.edu>
2018-11-02 13:54:16 -0400

    Car Factory created

--- database/factories/CarFactory.php
+++ database/factories/CarFactory.php
@@ -1,9 +1,13 @@
 <?php
-
+//faker for car database
 use Faker\Generator as Faker;
 
-$factory->define(Model::class, function (Faker $faker) {
+$factory->define(App\Car::class, function (Faker $faker) {
+    $random=mt_rand(0,2);
+    $CarsArray = array("Ford", "Honda", "Toyota");
     return [
-        //
+        'make' => $CarsArray[$random],
+        'model' => $faker->company,
+        'year' => $faker->year,
     ];
 });
