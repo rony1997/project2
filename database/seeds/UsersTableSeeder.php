c3af5f2004414e157835016163c5084b54bdf5aa
rony1997 <rad59@njit.edu>
2018-11-02 13:45:24 -0400

    Initial setup completed

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //user factory database
+        factory(App\User::class, 50)->create()->each(function ($u) {
+            // $u->posts()->save(factory(App\Post::class)->make());
+        });
+    }
+}
