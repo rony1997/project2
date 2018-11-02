6674e81437639a8cd4526d84ae715772e180e726
rony1997 <rad59@njit.edu>
2018-11-02 14:27:12 -0400

    Home page updated

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -24,5 +26,12 @@ class HomeController extends Controller
     public function index()
     {
         return view('home');
+    }
+    public function profile()
+    {
+        // $user=Auth::user();
+        $users=User::all();
+        // dd($users);
+        return view('profile',['users'=>$users]);
     }
 }
