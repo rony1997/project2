864c8c79728888db3848856b597dd3e30b5f518c
rony1997 <rad59@njit.edu>
2018-11-02 14:49:53 -0400

    Update User Test Done

--- tests/Unit/UpdateTest.php
+++ tests/Unit/UpdateTest.php
@@ -5,17 +5,18 @@ use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use App\User;
 use App\Car;
-class CountTest extends TestCase
+class UpdateTest extends TestCase
 {
     /**
      * A basic test example.
      *
      * @return void
      */
-    public function testUserCount()
+    public function testUpdateUser()
     {
-        $users=User::all();
-        $users->count();
-        $this->assertEquals(51,count($users),"Should return 50 users");
+        $user = User::find(5);
+        $user->name = 'Steve Smith';
+        $user->save();
+        $this->assertDatabaseHas('users', ["name" => "Steve Smith"]);
     }
 }
